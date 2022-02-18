494193a5eceeb022ec9f71a1fc41463cf95f5985
Ricardorouge <ricky032929@gmail.com>
2022-02-18 09:06:49 -0500

    created a character state that is set from an api upon first rendor of page

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,11 @@
 // Write your Character component here
+export default function Character(props){
+const {characters} = props;
+console.log(characters)
+    return(
+        <div>
+
+        </div>
+    )
+
+}
