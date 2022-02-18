57cd6670c1dd1baeb15b4573711e3430297f5e8e
Ricardorouge <ricky032929@gmail.com>
2022-02-18 10:12:13 -0500

    set state to empty array instead of null...  fixed everything

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-  const [characters,setCharacters] = useState(null);
+  const [characters,setCharacters] = useState([]);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
--- src/components/Character.js
+++ src/components/Character.js
@@ -3,16 +3,23 @@ import React from 'react';
 import styled from 'styled-components';
 
 const H1Style = styled.h1`
-    color:#e7ecef;
+    color:#0a2342;
+    border:solid 2px black;
+    width:20%;
+    margin:auto;
+    padding:10px;
 `;
 
 export default function Character(props){
+
 const {characters} = props;
+
 console.log(characters)
+
     return(
         <div>
             { characters.map((ch,idx)=>{
-                return(<H1Style key ={idx}>{ch.name} </H1Style>)
+                return(<H1Style key ={idx}>{ch.name}</H1Style>)
             })}   
         </div>
     )
