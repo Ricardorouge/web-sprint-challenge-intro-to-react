ec173021496a0c206983c4a734a3cf0a7dee9ee1
Ricardorouge <ricky032929@gmail.com>
2022-02-18 09:43:22 -0500

    mapped over characters and rendored each name into the dom

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,10 +1,19 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const H1Style = styled.h1`
+    color:#e7ecef;
+`;
+
 export default function Character(props){
 const {characters} = props;
 console.log(characters)
     return(
         <div>
-
+            { characters.map((ch,idx)=>{
+                return(<H1Style key ={idx}>{ch.name} </H1Style>)
+            })}   
         </div>
     )
 
